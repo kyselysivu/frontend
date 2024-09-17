9c6dd18dd99c615b46d615007ef26a0a9dcc86a5
Ananazzo <48922919+Ananazzo@users.noreply.github.com>
2024-09-17 10:32:05 +0300

    home page

--- src/sites/home.jsx
+++ src/sites/home.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import "./home.css"
+import { useState } from 'react';
 
 function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of the application.</p>
-    </div>
-  );
+    const [inputValue, setInputValue] = useState('');
+
+    const handleButtonClick = () => {
+      console.log(inputValue); // Replace this with your desired action
+    };
+    return (
+        <div>
+            <nav></nav>
+            <div class="flex-container">
+                <div class="text-container"> 
+                    <h1>Kyselysivu</h1>
+                    <p>
+                        Vastaa oikein sarjaan monivalintakysymyksiä ryhmäsi kanssa.
+                        Sinulla on yhteensä 12 minuuttia aikaa.
+                        Parhaat tulokset pääsevät kunniataululle!
+                    </p>
+                </div>
+                <div class="container">
+                    <input 
+                    type="text" 
+                    value={inputValue} 
+                    onChange={(e) => setInputValue(e.target.value)} 
+                    placeholder="Ryhmän nimi"
+                    />
+                    <button onClick={handleButtonClick}>Pelaa</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
