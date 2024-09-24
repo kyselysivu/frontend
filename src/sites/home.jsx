fb34aadd887a5fa08d8f5a4e6cbc89106ef28337
Ananazzo <48922919+Ananazzo@users.noreply.github.com>
2024-09-24 15:15:41 +0300

    Update home.jsx

--- src/sites/home.jsx
+++ src/sites/home.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import "./home.css"
+import "./home.css";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 
 function Home() {
     const navigate = useNavigate();
-
     const ToLeaderBoard = () => {
-      navigate('/leaderboard');
+        navigate('/leaderboard');
     };
 
     const [inputValue, setInputValue] = useState('');
     const [cookies, setCookie] = useCookies(['user']);
 
     const handleButtonClick = () => {
-      console.log(inputValue);
-      setCookie('user', inputValue, { sameSite: 'none', secure: false });
-      setCookie('team', '', { expires: new Date(0) });
-
-      console.log("cookie:", cookies.user);
-      navigate('/questions');
+        console.log(inputValue);
+        setCookie('user', inputValue, { sameSite: 'none', secure: false });
+        setCookie('team', '', { expires: new Date(0) });
+        console.log("cookie:", cookies.user);
+        navigate('/questions');
     };
+
     return (
         <div>
             <nav></nav>
             <div className="flex-container">
-                <div className="text-container"> 
+                <div className="text-container">
                     <h1 className="h1">Kyselysivu</h1>
                     <p className="p">
                         Vastaa oikein sarjaan monivalintakysymyksiä ryhmäsi kanssa.
@@ -36,13 +35,19 @@ function Home() {
                     <button className='kunnia' onClick={ToLeaderBoard}>Kunniataulukko</button>
                 </div>
                 <div className="container">
-                    <input 
-                        type="text" 
-                        value={inputValue} 
+                    <input
+                        type="text"
+                        value={inputValue}
                         onChange={(e) => setInputValue(e.target.value)}
                         placeholder="Ryhmän nimi"
                     />
-                    <button className="play-button" onClick={handleButtonClick}>Pelaa</button>
+                    <button 
+                        className="play-button" 
+                        onClick={handleButtonClick} 
+                        disabled={!inputValue} // Disable button if inputValue is empty
+                    >
+                        Pelaa
+                    </button>
                     <a href="/leaderboard" className='kunnialinkki'>Kunniataulukko</a>
                 </div>
             </div>
